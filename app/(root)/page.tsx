c15f27e435c056bf94dc7a6c2cf92f642f8facca
Daniel <daniel_ye.nz@hotmail.com>
2024-11-03 23:16:48 -0800

    finish search form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,23 @@
-export default function Home() {
+import SearchForm from "../../components/SearchForm";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
+
   return (
     <>
       <section className="pink_container">
-        <h1 className="heading">Pitch Your Startup, <br /> Connect With Entrepreneurs</h1>
-        <p className="sub-heading !max-w-3xl">Submit ideas, Vote on Pitches, Get Noticed in Virtual Competitions.</p>
+        <h1 className="heading">
+          Pitch Your Startup, <br /> Connect With Entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit ideas, Vote on Pitches, Get Noticed in Virtual Competitions.
+        </p>
+
+        <SearchForm query={query} />
       </section>
     </>
   );
