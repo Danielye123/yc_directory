a18abaff979fea3c013e1d9674e8dac11e063d3d
Daniel <daniel_ye.nz@hotmail.com>
2024-11-05 22:46:18 -0800

    finish startup card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import StartupCard from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
 
 export default async function Home({
@@ -6,6 +7,17 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+
+  const posts = [{
+    _createAt: new Date(),
+    views: 55,
+    author: { _id: 1, name: 'Daniel'},
+    _id: 1,
+    description: 'This is a description',
+    image: "https://media.wired.com/photos/6595c546f6145f9ca1309145/1:1/w_1600,h_1600,c_limit/_BC-wired_selmasabanovic_kaylareeferphoto-127.jpg",
+    category: "Robots",
+    title: "We Robots",
+  }]
 
   return (
     <>
@@ -23,6 +35,16 @@ export default async function Home({
         <p className="text-30-semibold">
           {query ? `Search results for ${query}` : 'All Startups'}
         </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartUpCardType, index: number) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No Startups Found</p>
+          )}
+        </ul>
       </section>
     </>
   );
